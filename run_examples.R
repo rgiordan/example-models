4778bbcb13be393de9ba3116d1e3e3a5d5d5d5e5
Ryan Giordano <rgiordan@gmail.com>
2017-05-22 17:19:13 -0700

    Generalize python script

--- run_examples.R
+++ run_examples.R
@@ -1,43 +1,41 @@
 library(rstan)
 
-
 rstan_options(auto_write = TRUE)
 
 #model_name <- "basic_estimators/bernoulli"
 #model_name <- "basic_estimators/normal_censored"
-model_name <- "sensitivity_generator/normal_censored"
+model_name <- "sensitivity_generator/normal_censored/normal_censored"
 
 model <- stan_model(paste(model_name, "stan", sep="."))
 
 stan_data <- new.env()
 source(paste(model_name, "data.R", sep="."), local=stan_data)
-stan_data$weights <- rep(1.0, stan_data$N_observed)
 
+# Get the standard draws.
 # Use chains=1 for now to avoid confusion around get_inits.
 num_samples <- 500
 result <- sampling(model, data=stan_data, chains=1, iter=num_samples * 2)
 print(result)
-result_draws <- extract(result, inc_warmup=FALSE)
 
+# Get the sensitivity model.
 model_sens <- stan_model(paste(model_name, "_sensitivity.stan", sep=""))
 model_sens_fit <- stan(paste(model_name, "_sensitivity.stan", sep=""),
                        data=stan_data, algorithm="Fixed_param", iter=1, chains=1)
 
-par_list <- get_inits(result, iter=100)[[1]]
-par_list$weights <- stan_data$weights
-par_list$y_var <- stan_data$y_var
-
-# Sanity check that get_inites is doing what I think it is.
-if (FALSE) {
-  mu_inits <- unlist(sapply(1:num_samples,
-                            function(n) { get_inits(result, iter=n + num_samples)[[1]]$mu[1] }))
-  plot(sort(mu_inits), sort(result_draws$mu))
-  abline(0, 1)
+# Get the sensitivity parameters in list form.
+sens_par_list <- get_inits(model_sens_fit)[[1]]
+for (par in names(sens_par_list)) {
+  if (par %in% names(stan_data)) {
+    cat("Copying hyperparameter ", par, " from the data.\n")
+    sens_par_list[[par]] <- stan_data[[par]]
+  }
 }
 
+# These names help sort through the vectors of sensitivity.
 param_names <- result@.MISC$stan_fit_instance$unconstrained_param_names(FALSE, FALSE)
 sens_param_names <- model_sens_fit@.MISC$stan_fit_instance$unconstrained_param_names(FALSE, FALSE)
 
+# Get the model gradients.
 grad_mat <- matrix(NA, num_samples, length(sens_param_names))
 prog_bar <- txtProgressBar(min=1, max=num_samples, style=3)
 for (n in 1:num_samples) {
@@ -46,17 +44,34 @@ for (n in 1:num_samples) {
   for (par in ls(par_list)) {
     # get_inits is broken
     # https://github.com/stan-dev/rstan/issues/417
-    par_list[[par]] <- as.numeric(par_list[[par]])
+    sens_par_list[[par]] <- as.numeric(par_list[[par]])
   }
-  par_list$weights <- stan_data$weights
-  par_list$y_var <- stan_data$y_var
-  pars_free <- unconstrain_pars(model_sens_fit, par_list)
+  pars_free <- unconstrain_pars(model_sens_fit, sens_par_list)
   grad_mat[n, ] <- grad_log_prob(model_sens_fit, pars_free)
 }
 close(prog_bar)
 
-weight_rows <- grepl("weights", sens_param_names)
-
 draws_mat <- extract(result, permute=FALSE)[,1,]
 sens_mat <- cov(grad_mat, draws_mat)
+
+
+
+######################
+# Sanity checks and misc code
+
+weight_rows <- grepl("weights", sens_param_names)
 plot(stan_data$y, sens_mat[weight_rows, 1])
+
+var_row <- grepl("y_var", sens_param_names)
+sens_mat[var_row, 1]
+
+mean(draws_mat[, 1])
+
+# Sanity check that get_inits is doing what I think it is.
+if (FALSE) {
+  mu_inits <- unlist(sapply(1:num_samples,
+                            function(n) { get_inits(result, iter=n + num_samples)[[1]]$mu[1] }))
+  plot(sort(mu_inits), sort(result_draws$mu))
+  abline(0, 1)
+}
+
--- sensitivity_generator/generate_models.py
+++ sensitivity_generator/generate_models.py
@@ -1,72 +1,103 @@
+import argparse
 import os
 
-data_block = """
-real U;
-int<lower=0> N_censored;
-int<lower=0> N_observed;
-real<upper=U> y[N_observed];
-"""
+# data_block = """
+#     real U;
+#     int<lower=0> N_censored;
+#     int<lower=0> N_observed;
+#     real<upper=U> y[N_observed];
+# """
+#
+# # TODO: strip constraints out of the hyperparameters.
+# hyperparameters_block = """
+#     real weights[N_observed];
+#     real y_var;
+# """
+#
+# parameters_block = """
+#     real mu;
+# """
+#
+#
+# model_block = """
+# for (n in 1:N_observed) {
+#   target += weights[n] * (
+#     normal_lpdf(y[n] | mu, y_var) - normal_lcdf(U | mu, y_var));
+# }
+# target += N_censored * log1m(normal_cdf(U, mu, y_var));
+# """
+#
+# extra_blocks = """
+# """
 
-# TODO: strip constraints out of the hyperparameters.
-hyperparameters_block = """
-real weights[N_observed];
-real<lower=0> y_var;
-"""
 
-parameters_block = """
-real mu;
-"""
+parser = argparse.ArgumentParser(
+    description='Generate stan models for sensitivity analysis.')
+parser.add_argument('--model_name', help='Directory and base filename for stan scripts.')
+args = parser.parse_args()
+
+f = open(args.model_name + '_data_block.stanblock', 'r')
+data_block = f.read()
+f.close()
+
+f = open(args.model_name + '_model_block.stanblock', 'r')
+model_block = f.read()
+f.close()
+
+f = open(args.model_name + '_parameters_block.stanblock', 'r')
+parameters_block = f.read()
+f.close()
+
+f = open(args.model_name + '_hyperparameters_block.stanblock', 'r')
+hyperparameters_block = f.read()
+f.close()
+
+extra_blocks_fname = args.model_name + '_extra_blocks.stanblock'
+if os.path.isfile(extra_blocks_fname):
+    f = open(extra_blocks_fname, 'r')
+    extra_blocks = f.read()
+    f.close()
+else:
+    extra_blocks = ''
 
 
-model_block = """
-for (n in 1:N_observed) {
-  target += weights[n] * (
-    normal_lpdf(y[n] | mu, y_var) - normal_lcdf(U | mu, y_var));
-}
-target += N_censored * log1m(normal_cdf(U, mu, y_var));
-"""
-
-extra_blocks = """
-"""
-
-out_dir = '/home/rgiordan/Documents/git_repos/example-models/sensitivity_generator'
-model_name = 'normal_censored'
-
-f = open(os.path.join(out_dir, model_name + '.stan'), 'w')
+f = open(os.path.join(args.model_name + '.stan'), 'w')
 
 f.write('data {\n')
 f.write(data_block)
 f.write(hyperparameters_block)
-f.write('}\n')
+f.write('\n}\n')
 
 f.write('parameters {\n')
 f.write(parameters_block)
-f.write('}\n')
+f.write('\n}\n')
 
 f.write('model {\n')
 f.write(model_block)
-f.write('}\n')
+f.write('\n}\n')
 
 f.write(extra_blocks)
+f.write('\n')
 
 f.close()
 
 
-f = open(os.path.join(out_dir, model_name + '_sensitivity.stan'), 'w')
+f = open(os.path.join(args.model_name + '_sensitivity.stan'), 'w')
 
 f.write('data {\n')
 f.write(data_block)
-f.write('}\n')
+f.write('\n}\n')
 
 f.write('parameters {\n')
 f.write(parameters_block)
 f.write(hyperparameters_block)
-f.write('}\n')
+f.write('\n}\n')
 
 f.write('model {\n')
 f.write(model_block)
-f.write('}\n')
+f.write('\n}\n')
 
 f.write(extra_blocks)
+f.write('\n')
 
 f.close()
